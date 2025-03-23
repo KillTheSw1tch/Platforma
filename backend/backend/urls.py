f0d61c55fe4a9306bcf38175316cca1da4626131
KillTheSw1tch <zaharpetrenko525@gmail.com>
2025-03-23 16:46:31 +0100

    Add existing project files to Git

---
+++ backend/backend/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic import TemplateView
+from api.views import CreateUserView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/user/register/", CreateUserView.as_view(), name="register"),
+    path("api/token/", TokenObtainPairView.as_view(), name="get_token"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="refresh"),
+    path("api-auth/", include("rest_framework.urls")),
+    path('', TemplateView.as_view(template_name="api/home.html"), name='home'),
+    path('registration/', TemplateView.as_view(template_name='api/registrationCarrier.html'), name='registration'),
+    path('login/', TemplateView.as_view(template_name='api/login.html'), name='login'),
+    path('add-cargo/', TemplateView.as_view(template_name='api/add_cargo.html'), name='add_cargo'),
+]
+
